0b37671765ef942cac31f8b58a84eaf8441e0bb9
FUJI <jazzmaster204@gmail.com>
2026-08-25 16:36:22 +0300

    [SPRING] {MVC} -practice- :"add Animal Service to project"

--- src/main/java/com/example/service/AnimalService.java
+++ src/main/java/com/example/service/AnimalService.java
@@ -1,4 +1,15 @@
 package com.example.service;
 
+import com.example.entity.Animal;
+
+import java.util.List;
+
 public interface AnimalService {
+
+    List<Animal> findAll ();
+    Animal findById (int id);
+    void delete (Animal animal);
+    void update (Animal animal);
+    void save (Animal animal);
+
 }
--- src/main/java/com/example/service/AnimalServiceImpl.java
+++ src/main/java/com/example/service/AnimalServiceImpl.java
@@ -1,4 +1,42 @@
 package com.example.service;
 
+import com.example.dao.AnimalDAO;
+import com.example.entity.Animal;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class AnimalServiceImpl {
+
+    private AnimalDAO animalDAO;
+
+    @Autowired
+    public AnimalServiceImpl (AnimalDAO animalDAO) {
+        this.animalDAO = animalDAO;
+    }
+
+    public List<Animal> findAll () {
+        return animalDAO.findAll();
+    }
+
+    public Animal findById(int id) {
+        return animalDAO.findById(id);
+    }
+
+    public void save (Animal animal) {
+        animalDAO.save(animal);
+    }
+
+    public void delete (Animal animal) {
+        animalDAO.delete(animal);
+    }
+
+    public void update (Animal animal) {
+        animalDAO.update(animal);
+    }
+
 }
