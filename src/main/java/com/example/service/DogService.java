0d30aaeeb5805af4540d6e12abd63e63df64c51c
FUJI <jazzmaster204@gmail.com>
2026-08-31 13:46:05 +0300

    [SPRING MVC] "make Service"

--- src/main/java/com/example/service/DogService.java
+++ src/main/java/com/example/service/DogService.java
@@ -1,4 +1,14 @@
 package com.example.service;
 
+import com.example.entity.Dog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface DogService {
+    List<Dog> showAll ();
+    Dog showDog (int id);
+    void update (Dog dog);
+    void delete (Dog dog);
+    void save (Dog dog);
 }
--- src/main/java/com/example/service/DogServiceImpl.java
+++ src/main/java/com/example/service/DogServiceImpl.java
@@ -1,4 +1,45 @@
 package com.example.service;
 
+import com.example.dao.DogDAO;
+import com.example.entity.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DogServiceImpl implements DogService {
-}
+
+    private final DogDAO dogDAO;
+
+    @Autowired
+    public DogServiceImpl (DogDAO dogDAO) {
+        this.dogDAO = dogDAO;
+    }
+
+    @Override
+    public List<Dog> showAll () {
+        return dogDAO.showAll();
+    }
+
+    @Override
+    public Dog showDog (int id) {
+        return dogDAO.showDog(id);
+    }
+
+    @Override
+    public void save (Dog dog) {
+        dogDAO.save(dog);
+    }
+
+    @Override
+    public void update (Dog dog) {
+        dogDAO.update(dog);
+    }
+
+    @Override
+    public void delete (Dog dog) {
+        dogDAO.delete(dog);
+    }
+
+ }
