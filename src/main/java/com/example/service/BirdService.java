4493a0615688098bb77749cca41463c7d2d277d9
FUJI <jazzmaster204@gmail.com>
2026-09-09 11:13:10 +0300

    [SPRING MVC] "Bird service + DAO"

--- src/main/java/com/example/service/BirdService.java
+++ src/main/java/com/example/service/BirdService.java
@@ -1,4 +1,13 @@
 package com.example.service;
 
+import com.example.entity.Bird;
+
+import java.util.List;
+
 public interface BirdService {
+    Bird getBird (int id);
+    List<Bird> getBirds ();
+    void updateBird (Bird bird);
+    void deleteBird (Bird bird);
+    void saveBird (Bird bird);
 }
--- src/main/java/com/example/service/BirdServiceImpl.java
+++ src/main/java/com/example/service/BirdServiceImpl.java
@@ -1,4 +1,49 @@
 package com.example.service;
 
-public class BirdServiceImpl {
+import com.example.dao.BirdDAO;
+import com.example.entity.Bird;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class BirdServiceImpl implements BirdService{
+
+    private final BirdDAO birdDAO;
+
+    @Autowired
+    public BirdServiceImpl (BirdDAO birdDAO) {
+        this.birdDAO = birdDAO;
+    }
+
+    @Override
+    public Bird getBird (int id) {
+        return birdDAO.getBird(id);
+    }
+
+    @Override
+    public List<Bird> getBirds () {
+        return birdDAO.getBirds();
+    }
+
+    @Override
+    public void updateBird (Bird bird) {
+        birdDAO.updateBird(bird);
+    }
+
+    @Override
+    public void deleteBird (Bird bird) {
+        birdDAO.deleteBird(bird);
+    }
+
+    @Override
+    public void saveBird (Bird bird) {
+        birdDAO.saveBird(bird);
+    }
+
+
+
 }
